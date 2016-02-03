b8caf131b24e429f83af73055cd676bc731a1aba
Aditya Nivarthi <adityanivarthi19@gmail.com>
2016-02-03 17:18:44 -0500

    Comments in Main.

--- src/main/java/edu/wpi/cs/c4fe/Main.java
+++ src/main/java/edu/wpi/cs/c4fe/Main.java
@@ -7,12 +7,20 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class is the main class for running the connect 4 feature evaluator.
+ * @author Aditya Nivarthi
+ */
 public class Main {
 
     public static final int BOARD_WIDTH = 7;
     public static final int BOARD_HEIGHT = 6;
     public static final int CONNECT_LENGTH = 4;
 
+    /**
+     * Main method.
+     * @param args Command line arguments.
+     */
     public static void main(String[] args) {
         if (args.length != 2) {
             System.out.println("Incorrect number of arguments.");
@@ -21,25 +29,31 @@ public class Main {
         String inputFile = args[0];
         String outputFile = args[1];
 
-        List<GameState> states = Main.read(inputFile);
-        for (GameState e : states) {
-            System.out.println(e.toString() + "\n");
-        }
+        List<GameState> inputStates = Main.read(inputFile);
     }
 
+    /**
+     * Returns the list of {@link GameState}s from the specified input file.
+     * @param filename The name of the input file to read.
+     * @return a List&lt;GameState&gt;
+     */
     protected static List<GameState> read(String filename) {
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
 
             String input;
+
+            // Chew header line in file
             bufferedReader.readLine();
 
-            List<GameState> states = new ArrayList<GameState>();
+            List<GameState> states = new ArrayList<>();
 
+            // Each line of input is a new GameState
             while ((input = bufferedReader.readLine()) != null) {
                 String[] positions = input.split(",");
                 BoardCell[][] gameBoard = new BoardCell[BOARD_WIDTH][BOARD_HEIGHT]; // x firs increase, y decrease
 
+                // Set the board values from the positions in the input
                 try {
                     int index = 0;
                     for (int i = 0; i < BOARD_WIDTH; i++) {
@@ -53,6 +67,7 @@ public class Main {
                     break;
                 }
 
+                // Create a new GameState from the board
                 GameState state = new GameState(CONNECT_LENGTH, gameBoard, new boolean[]{false, false});
                 states.add(state);
             }
